e992205233f8a28e7f094d4ba5cf903b6143c2cd
Perkoz19 <48656967+Perkoz19@users.noreply.github.com>
2019-03-17 21:57:33 +0100

    VideoHandler v1.0
    VIdeoHandler v1.0 by xXx_DevastatoR_xXx
    (c) 2019

--- video.py
+++ video.py
@@ -1,10 +1,25 @@
 import cv2
+import os
 
-class VideoHandler():
-    
-    
+
+class VideoHandler:
     def __init__(self):
         pass
 
     def get_frame(self):
-        pass
+        cap = cv2.VideoCapture(0)
+        count = 0
+        path = 'C:/users/wasik/Desktop/obrazki'  # sciezka do wpisania gdzie maja byc obrazki zapisane
+
+        while True:
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('frame',gray)
+            cv2.imwrite(os.path.join(path, "frame%d.jpg" % count), frame)
+            count += 1
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
